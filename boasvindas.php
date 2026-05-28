ec1b8e8bb26a156a20c850da0ad8cdf6a75297ec
André Nonaka <nonaka.andre@gmail.com>
2026-05-28 15:25:28 -0300

    adicionando php

---
+++ boasvindas.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br" data-bs-theme="dark">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="boot.css">
+    <title>Iniciar Sessão</title>
+</head>
+
+<body class="d-flex align-items-center py4 bg-body-tertiary">
+    <main class="w-100 m-auto form-container">
+        <div class="card" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">Bem-vindo ao MathCard!</h5>
+                <p class="card-text">
+                    Entre em uma batalha matemática cheia de desafios, estratégia e raciocínio rápido. Acha que domina
+                    os números? Prove isso — mas cuidado, este jogo pode te deixar de cabelos brancos!</p>
+                <a href="jogo.html" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+    </main>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
